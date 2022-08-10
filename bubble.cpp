a13bb7bbc2d17619de783d993be1d4dd383daf75
Esmeralda Godinez <egodinezm1600@alumno.ipn.mx>
2022-08-09 21:34:49 -0500

    create sort() function

--- bubble.cpp
+++ bubble.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 using namespace std;
 
+int set[] = {1,0};
 //Bubble sort
+auto sort()->void{
+  int tam = sizeof set / sizeof set[0];
+  for(int i = 0; i < tam; i++){ //Each element has to find it´s place in the array
+    for(int j = 0, k = 1; j < tam - 1; j++, k++){//Swapping 
+      if(set[j]>set[k]){
+        int aux = set[j];
+        set[j] = set[k];
+        set[k] = aux;
+      }
+    }
+  }
+}
+
 int main(){
+  sort();
+  for(int i: set){
+    cout<<i<<" ";
+  }
   return 0;
 }
